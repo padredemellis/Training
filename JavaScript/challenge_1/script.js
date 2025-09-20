b0bcfb68b15d44ecd320d606157c77788b4a494a
root <root@emadev.>
2025-09-20 20:11:14 -0300

    challenge_1

--- JavaScript/challenge_1/script.js
+++ JavaScript/challenge_1/script.js
@@ -1,17 +1,36 @@
+let timeOver;
+let timeInterval;
+
 function showFinishTime(){
-    setTimeout(timeOver, 1000 * 30);
-}
-function timeOver(){
-    let sound = document.getElementById("alarm");
-    alert("Game Over");
-    sound.play();
-}
-function startClock(){
-    setInterval(ticTac,1000);
+    timeOver = setTimeout(timeFinish, 1000*30);
+    timeInterval = setInterval(ticTac,1000);
+
+    document.getElementById("countdown").textContent = 30;
 }
 function ticTac(){
-    let timeActual = new Date();
-    let hour = timeActual.getHours();
-    let minutes = timeActual.getMinutes();
-    let seconds = timeActual.getSeconds();
+    let time = document.getElementById("countdown").textContent;
+    document.getElementById("countdown").textContent = time -1;
+}
+function timeFinish(){
+    clearInterval(timeInterval);
+    document.getElementById("countdown").textContent = 0;
+    document.getElementById("alarm").play();
+    alert("Game Over:  time is finish, Try Again");
+}
+function finish(){
+    clearTimeout(timeOver);
+    clearInterval(timeInterval);
+    let date = new Date();
+    let answer1 = document.getElementById("Answer_1").value;
+    let answer2 = document.getElementById("Answer_2").value;
+    let answer3 = document.getElementById("Answer_3").value;
+    let answer4 = document.getElementById("Answer_4").value;
+    let answer5 = document.getElementById("Answer_5").value;
+
+    let message = `${date.toLocaleDateString("es-Es")}
+    \n 1: ${answer1}\n 2: ${answer2}\n 3: ${answer3}\n 4: ${answer4}\n 5: ${answer5}`;
+    alert(message);
+}
+function tryAgain(){
+    location.reload();
 }
